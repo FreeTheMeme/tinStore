1f7f10fba8368e9731f56c07aedb5e45f9e92a0e
bigtin <bigtin@protonmail.com>
2025-08-13 01:31:25 -0400

    db insert working

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 # db.py
 # any data base comands will be put here
-
+#
+import mysql.connector
 #TABLES
     # item table 
     # barcode id or just id
@@ -13,3 +14,25 @@
     # name 
     # rank
     # password (hased)
+
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="root",
+  password="example",
+  database="tinstore",
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS players (name VARCHAR(255), uuid VARCHAR(255), time_played INT(11))")     
+
+def insert_item(barcode, name, notes=None, sold=False):
+
+  sql = """
+  INSERT INTO items (barcode, name, notes, sold)
+  VALUES (%s, %s, %s, %s)
+  """
+  values = (barcode, name, notes, sold)
+  mycursor.execute(sql, values)
+  mydb.commit()
+  print(f'|DB| added {name} to db')
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # main.py 
 # applaction loop will be written here
-
+import db
 # start, the user will be promted with potions susch as:
 # scan barcode lookup
 # scan barcode add to db
+db.insert_item('12345','cool-item')
 # create barcode -> if yes ask to print
 # view invtory
