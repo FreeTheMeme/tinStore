226d2ff42c3d570d88bf54ea4af7f8c1e62dd398
bigtin <bigtin@protonmail.com>
2025-08-16 16:26:58 -0400

    more cli

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import barcode
 import db
 
+
+
+def add_barcode():
+    print('enter name:')
+    cli_name = input()
+    print('scan barcode or enter manuly')
+    cli_barcode = input()
+    print(f'adding {cli_name} \nwith barcode:{cli_barcode} ')
+    db.insert_item(cli_barcode,cli_name,'added with cli')
+    print('print y/n')
+def lookup_barcode():
+    print('enter barcode')
+    cli_barcode_l = input()
+    print(db.lookup_item())
+
+
+
 print('=====TinStore CLI v0.1=====')
-print('plese chouse and option')
-print("""
-\t 1 add item and print
-\t 2 lookup item
-\t 0 quit
-""")
 
 running = True
 # opt sector
 while running == True:
+    print('plese chouse and option')
+    print("""
+    \t 1 add item and print
+    \t 2 lookup item
+    \t 0 quit
+    """)
+    print('enter a number')
     option = input()
 
     if int(option) == 1:
         print('add item')
+        add_barcode()
     elif int(option) == 2:
         print('look up item')
+        lookup_barcode()
     elif int(option) == 0:
         print('quit bye')
         running = False
@@ -27,3 +47,4 @@ while running == True:
         print('not an option')
     option = 99
 print(running)
+
